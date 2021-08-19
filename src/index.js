789a3b35e8d66f5043f279937e97594e068dfdc4
Rexxar1919 <vladimir.popov1998@mail.ru>
2021-08-19 12:59:11 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == null ) {
+    return 0;
+  }
+  let search = 0;
+  for (let biba = 0; biba < array.length; biba++) {
+    if (array[biba] < search) {
+      search = array[biba];
+    }
+    
+  }
+  return search;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null ) {
+    return 0;
+  }
+  let search = 0;
+  for (let biba = 0; biba < array.length; biba++) {
+    if (array[biba] > search) {
+      search = array[biba];
+    }
+    
+  }
+  return search;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == NaN || array == null || array.length == 0) {
+    return 0;
+  }
+  let sum = 0, avgSum = 0;
+  for (let biba = 0; biba < array.length; biba++) {
+    sum += array[biba];
+  }
+  avgSum = sum / array.length
+  return avgSum;
 }
